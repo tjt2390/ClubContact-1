30948fe1fa1e6f2c9663c030a2b09a4db3f19f0d
Sara Laufers <slaufers001@csbsju.edu>
2019-10-22 12:04:04 -0500

    Redoing Homepage
    This commit is redoing the homepage because the items are unclickable and  shared classes are very inflexible with firestore

--- EventsTrackerApp/app/src/main/java/com/example/eventstrackerapp/ui/home/HomeFragment.java
+++ EventsTrackerApp/app/src/main/java/com/example/eventstrackerapp/ui/home/HomeFragment.java
@@ -4,16 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.eventstrackerapp.FirebaseDatabaseHelper;
 import com.example.eventstrackerapp.R;
 
 public class HomeFragment extends Fragment {
@@ -27,8 +23,7 @@ public class HomeFragment extends Fragment {
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
 
-        mRecyclerView = (RecyclerView) root.findViewById(R.id.recyclerview_events);
-        //new FirebaseDatabaseHelper()
+
 
         return root;
     }
